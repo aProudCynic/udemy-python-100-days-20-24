d7f31bf20202815333bc3d18cf9f0641c7c7afc4
Balázs Márton <bmarton87@gmail.com>
2021-04-13 21:33:21 +0200

    feature: implemented movements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import time
 
 class Snake:
 
@@ -15,16 +16,36 @@ class Snake:
             if(segment_number != 0):
                 segment.back(Snake.SNAKE_SEGMENT_SIZE * segment_number)
             self.segments.append(segment)
+        self.turning = False
 
     def move_forward(self):
         for segment in self.segments:
             segment.forward(Snake.SNAKE_SEGMENT_SIZE)
 
-    def turn_left(self):
-        for segment_number in range(len(self.segments)):
-            self.segments[segment_number].left(90)
-            for segment in self.segments:
-                segment.forward(Snake.SNAKE_SEGMENT_SIZE)
+    def up(self):
+        self.rotate(90)
+
+    def left(self):
+        self.rotate(180)
+
+    def down(self):
+        self.rotate(270)
+
+    def right(self):
+        self.rotate(360)
+
+    def rotate(self, new_heading):
+        opposite_of_current_heading = 180 - int(self.segments[0].heading())
+        if not self.turning and opposite_of_current_heading != new_heading:
+            self.turning = True
+            for segment_number in range(len(self.segments)):
+                self.segments[segment_number].setheading(new_heading)
+                for segment in self.segments:
+                    segment.forward(Snake.SNAKE_SEGMENT_SIZE)
+                screen.update()
+                time.sleep(1)
+            self.turning = False
+
 
 screen = Screen()
 screen.bgcolor('black')
@@ -34,10 +55,17 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
 game_on = True
 while game_on:
     snake.move_forward()
     screen.update()
+    time.sleep(1)
 
 screen.exitonclick()
 
