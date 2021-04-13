303710db9ea113e722c63bd9ea07e00dc68cfc97
Balázs Márton <bmarton87@gmail.com>
2021-04-13 20:55:37 +0200

    feture: snake created

---
+++ main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+class Snake:
+    def __init__(self, nr_of_segments):
+        self.segments = []
+        for segment_number in range(nr_of_segments):
+            segment = Turtle()
+            segment.shape('square')
+            segment.color('white')
+            segment.penup()
+            if(segment_number != 0):
+                segment.back(20 * segment_number)
+            self.segments.append(segment)
+
+screen = Screen()
+screen.bgcolor('black')
+
+snake = Snake(3)
+screen.exitonclick()
+
