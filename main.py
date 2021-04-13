8e3f01e106a56bb6ec678740dd1f1b20f9b02ac8
Balázs Márton <bmarton87@gmail.com>
2021-04-13 20:58:52 +0200

    feature: move snake forward

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ class Snake:
                 segment.back(20 * segment_number)
             self.segments.append(segment)
 
+    def move_forward(self):
+        for segment in self.segments:
+            segment.forward(20)
+
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(600, 600)
 
 snake = Snake(3)
+game_on = True
+while game_on:
+    snake.move_forward()
+
 screen.exitonclick()
 
